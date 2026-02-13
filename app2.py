a165e0fb81ede32a7504b6931b49be3173e41e3e
deepak78777 <deepak516421@gmail.com>
2026-02-13 15:23:46 +0530

    Revert "keys added in config"
    This reverts commit 999a01d103de077b58fb52bd987ef6a17f095385.

--- app2.py
+++ app2.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import os
+from dotenv import load_dotenv
 from groq import Groq
 import PyPDF2
 from docx import Document
-from config import GROQ_API_KEY, MODEL_NAME
+
+# Load environment variables
+load_dotenv()
+
+GROQ_API_KEY = "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"
+MODEL_NAME = "openai/gpt-oss-20b"
 
 client = Groq(api_key=GROQ_API_KEY)
 
